9e9899b90adfbafc2a3f1a5d4ff6508e7ff73868
lldan <safinildan@gmail.com>
2023-12-08 20:17:26 +0300

    3 задание

--- 3/index.ts
+++ 3/index.ts
@@ -1,19 +1,6 @@
 /*
-
-Intro:
-
-    Since we already have some of the additional
-    information about our users, it's a good idea
-    to output it in a nice way.
-
-Exercise:
-
-    Fix type errors in logPerson function.
-
-    logPerson function should accept both User and Admin
-    and should output relevant information according to
-    the input: occupation for User and role for Admin.
-
+Fix type errors in logPerson function.
+logPerson function should accept both User and Admin and should output relevant information according to the input: occupation for User and role for Admin.
 */
 
 interface User {
@@ -52,7 +39,7 @@ export const persons: Person[] = [
         role: 'World saver'
     }
 ];
-
+// = = =
 export function logPerson(person: Person) {
     let additionalInformation: string;
     if ("role" in person) {
@@ -62,8 +49,6 @@ export function logPerson(person: Person) {
     }
     console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
 }
-
+// = = =
 persons.forEach(logPerson);
 
-// In case you are stuck:
-// https://www.typescriptlang.org/docs/handbook/2/narrowing.html#the-in-operator-narrowing
