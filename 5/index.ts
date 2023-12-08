c332eccc871da85bc2a77a7f237bdc57aed08de3
lldan <safinildan@gmail.com>
2023-12-08 20:21:29 +0300

    5 задание

--- 5/index.ts
+++ 5/index.ts
@@ -1,24 +1,6 @@
 /*
-
-Intro:
-
-    Time to filter the data! In order to be flexible
-    we filter users using a number of criteria and
-    return only those matching all of the criteria.
-    We don't need Admins yet, we only filter Users.
-
-Exercise:
-
-    Without duplicating type structures, modify
-    filterUsers function definition so that we can
-    pass only those criteria which are needed,
-    and not the whole User information as it is
-    required now according to typing.
-
-Higher difficulty bonus exercise:
-
-    Exclude "type" from filter criteria.
-
+Without duplicating type structures, modify filterUsers function definition so that we can pass only those criteria which are needed, and not the whole User information as it is required now according to typing.
+Exclude "type" from filter criteria.
 */
 
 interface User {
@@ -84,7 +66,10 @@ export function logPerson(person: Person) {
     }
     console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
 }
+
+// = = =
 type UserCriteria = Partial<Omit<User, 'type'>>;
+// = = =
 
 export function filterUsers(persons: Person[], criteria:  UserCriteria ): User[] {
     return persons.filter(isUser).filter((user) => {
@@ -103,7 +88,3 @@ filterUsers(
         age: 23
     }
 ).forEach(logPerson);
-
-// In case you are stuck:
-// https://www.typescriptlang.org/docs/handbook/utility-types.html#partialtype
-// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html#predefined-conditional-types
